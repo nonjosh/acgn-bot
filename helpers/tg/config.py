bb5c56f0552bd9a361176f0389d0e0c88efb04e3
nonjosh <nonjosh@gmail.com>
2020-07-06 23:22:34 +0800

    minor comment changes

--- helpers/tg/config.py
+++ helpers/tg/config.py
@@ -6,4 +6,5 @@ class BotConfig:
 
 class ChannelConfig:
     # channel
+    # check by https://api.telegram.org/bot<YourBOTToken>/getUpdates
     chat_id = os.environ.get('CHAT_ID', '<your chat_id>')
