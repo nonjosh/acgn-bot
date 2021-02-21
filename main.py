3e9deb4bb97c20fa6d1e0b1a449481e5eecb3009
nonjosh <nonjosh@gmail.com>
2021-02-21 04:20:44 +0000

    change back to show simplified chin title

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ def wutuxsChecker(wutuxsHelper, show_no_update_msg=False):
     novel_name = wutuxsHelper.name
     if wutuxsHelper.checkUpdate():
         printT(
-            f"Update found for {wutuxsHelper.name}: {wutuxsHelper.latest_chapter_title_cht} ({wutuxsHelper.latest_chapter_url})"
+            f"Update found for {wutuxsHelper.name}: {wutuxsHelper.latest_chapter_title} ({wutuxsHelper.latest_chapter_url})"
         )
 
         tgHelper = TgHelper(token, chat_id)
         content = f"comic <<{novel_name}>> updated!"
-        url_text = f"<<{novel_name}>> {wutuxsHelper.latest_chapter_title_cht}"
+        url_text = f"<<{novel_name}>> {wutuxsHelper.latest_chapter_title}"
         tgHelper.send_channel(
             content=content,
             url_text=url_text,
@@ -39,12 +39,12 @@ def cocomanhuaChecker(cocomanhuaHelper, show_no_update_msg=False):
     comic_name = cocomanhuaHelper.name
     if cocomanhuaHelper.checkUpdate():
         printT(
-            f"Update found for {cocomanhuaHelper.name}: {cocomanhuaHelper.latest_chapter_title_cht} ({cocomanhuaHelper.latest_chapter_url})"
+            f"Update found for {cocomanhuaHelper.name}: {cocomanhuaHelper.latest_chapter_title} ({cocomanhuaHelper.latest_chapter_url})"
         )
 
         tgHelper = TgHelper(token, chat_id)
         content = f"comic <<{comic_name}>> updated!"
-        url_text = f"<<{comic_name}>> {cocomanhuaHelper.latest_chapter_title_cht}"
+        url_text = f"<<{comic_name}>> {cocomanhuaHelper.latest_chapter_title}"
         tgHelper.send_channel(
             content=content,
             url_text=url_text,
@@ -72,9 +72,6 @@ if __name__ == "__main__":
         CocomanhuaHelper(name="撿個校花做老婆", url="https://www.cocomanhua.com/12457/")
     )
     cocomanhuaHelperList.append(
-        CocomanhuaHelper(name="萬古神王", url="https://www.cocomanhua.com/12970/")
-    )
-    cocomanhuaHelperList.append(
         CocomanhuaHelper(name="總之就是非常可愛", url="https://www.cocomanhua.com/15006/")
     )
     cocomanhuaHelperList.append(
@@ -97,17 +94,16 @@ if __name__ == "__main__":
     )
     cocomanhuaHelperList.append(
         CocomanhuaHelper(name="石紀元", url="https://www.cocomanhua.com/14424/")
-    )    
+    )
 
     wutuxsHelperList = []
     wutuxsHelperList.append(
         WutuxsHelper(name="元尊", url="http://www.wutuxs.com/html/7/7876/")
     )
 
-
     for cocomanhuaHelper in cocomanhuaHelperList:
         printT(
-            f"Current chapter for comic {cocomanhuaHelper.name}: {cocomanhuaHelper.latest_chapter_title_cht} ({cocomanhuaHelper.latest_chapter_url})"
+            f"Current chapter for comic {cocomanhuaHelper.name}: {cocomanhuaHelper.latest_chapter_title} ({cocomanhuaHelper.latest_chapter_url})"
         )
         schedule.every(5).to(30).minutes.do(
             cocomanhuaChecker,
@@ -116,7 +112,7 @@ if __name__ == "__main__":
         )
     for wutuxsHelper in wutuxsHelperList:
         printT(
-            f"Current chapter for novel {wutuxsHelper.name}: {wutuxsHelper.latest_chapter_title_cht} ({wutuxsHelper.latest_chapter_url})"
+            f"Current chapter for novel {wutuxsHelper.name}: {wutuxsHelper.latest_chapter_title} ({wutuxsHelper.latest_chapter_url})"
         )
         schedule.every(5).to(30).minutes.do(
             wutuxsChecker,
