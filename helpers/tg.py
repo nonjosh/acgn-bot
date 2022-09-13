3112796d4f70fdec4d237fdcc27e3e4e2fff34bb
nonjosh <nonjosh@gmail.com>
2022-09-14 02:33:22 +0800

    remove idle function in TgHelper.run()

--- helpers/tg.py
+++ helpers/tg.py
@@ -55,11 +55,6 @@ class TgHelper:
         # Start the Bot
         self.updater.start_polling()
 
-        # Run the bot until you press Ctrl-C or the process receives SIGINT,
-        # SIGTERM or SIGABRT. This should be used most of the time, since
-        # start_polling() is non-blocking and will stop the bot gracefully.
-        self.updater.idle()
-
     def send_msg(
         self,
         content="No input content",
