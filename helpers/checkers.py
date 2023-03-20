1e75634409da45c1e2f338f7c6aa7cecadd80f70
Johnson Wong <nonjosh@gmail.com>
2023-03-20 23:35:16 +0800

    add SixNineShuChecker class in checkers.py

--- helpers/checkers.py
+++ helpers/checkers.py
@@ -287,6 +287,29 @@ class SyosetuChecker(AbstractChapterChecker):
         return chapter_list
 
 
+class SixNineShuChecker(AbstractChapterChecker):
+    """69shu checker class"""
+
+    def get_latest_chapter_list(self) -> List[Chapter]:
+        """Get latest chapter list
+
+        Returns:
+            List[Chapter]: latest chapter list
+        """
+        soup = self.get_latest_soup()
+        if not soup:
+            return []
+
+        dl_list = list(soup.find("div", {"class": "qustime"}).findAll("a"))
+        chapter_list = []
+        for chapter_tag in dl_list:
+            chapter_title = chapter_tag.find("span").text
+            chapter_url = chapter_tag["href"]
+            chapter_list.append(Chapter(title=chapter_title, url=chapter_url))
+
+        return chapter_list
+
+
 class ManhuaguiChecker(AbstractChapterChecker):
     """Manhuagui chapter list"""
 
