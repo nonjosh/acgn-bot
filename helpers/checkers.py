66149afad6b2e0b1f9317a676bc6a01a0eee6082
nonjosh <nonjosh@gmail.com>
2023-06-15 22:50:20 +0800

    update BaozimhChecker crawler

--- helpers/checkers.py
+++ helpers/checkers.py
@@ -453,20 +453,28 @@ class BaozimhChecker(AbstractChapterChecker):
         if soup is None:
             return []
 
-        a_list = [
-            a.findAll("a", {"class": "comics-chapters__item"})
-            for a in soup.select("div[class='pure-g']")
-        ]
-        chapter_list = []
-        for chapter_tag in a_list:
-            chapter_title = chapter_tag[0].text
-            chapter_path = chapter_tag[0]["href"]
-            chapter_url = urlunparse(
-                urlparse(self.check_url)._replace(path=chapter_path)
-            )
-            chapter_list.append(Chapter(title=chapter_title, url=chapter_url))
+        pure_g_div_list = soup.findAll("div", {"class": "pure-g"})
 
-        return chapter_list[::-1]
+        # Find which div contains chapter items
+        for pure_g_div in pure_g_div_list:
+            if pure_g_div.find("a", {"class": "comics-chapters__item"}):
+                a_list = [
+                    a.findAll("a", {"class": "comics-chapters__item"})
+                    for a in pure_g_div
+                ]
+                chapter_list = []
+                for chapter_tag in a_list:
+                    chapter_title = chapter_tag[0].text
+                    chapter_path = chapter_tag[0]["href"]
+                    chapter_url = urlunparse(
+                        urlparse(self.check_url)._replace(path=chapter_path)
+                    )
+                    chapter_list.append(
+                        Chapter(title=chapter_title, url=chapter_url)
+                    )
+
+                return chapter_list[::-1]
+        return []
 
 
 class XbiqugeChecker(AbstractChapterChecker):
