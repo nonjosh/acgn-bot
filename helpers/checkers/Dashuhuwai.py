68fe5a0b3810281d8cc495094031f890ca5e2596
nonjosh <nonjosh@gmail.com>
2023-09-24 13:09:02 +0800

    move checkers into distinct files

---
+++ helpers/checkers/Dashuhuwai.py
@@ -0,0 +1,29 @@
+from typing import List
+from urllib.parse import urlparse, urlunparse
+from helpers.chapter import Chapter
+from helpers.checkers.base import AbstractChapterChecker
+
+
+class DashuhuwaiChecker(AbstractChapterChecker):
+    """Dashuhuwai checker"""
+
+    def get_latest_chapter_list(self) -> List[Chapter]:
+        """Get latest chapter list from dashuhuwai
+
+        Returns:
+            List[Chapter]: latest chapter list
+        """
+        soup = self.get_latest_soup()
+        if soup is None:
+            return []
+        a_list = [li.find("a") for li in soup.find(id="ul_chapter1").find_all("li")]
+        chapter_list = []
+        for chapter_tag in a_list:
+            chapter_title = chapter_tag.text
+            chapter_path = chapter_tag["href"]
+            chapter_url = urlunparse(
+                urlparse(self.check_url)._replace(path=chapter_path)
+            )
+            chapter_list.append(Chapter(title=chapter_title, url=chapter_url))
+
+        return chapter_list
---
+++ helpers/checkers/Xbiquge.py
@@ -0,0 +1,32 @@
+from typing import List
+from urllib.parse import urljoin
+from helpers.chapter import Chapter
+from helpers.checkers.base import AbstractChapterChecker
+
+
+class XbiqugeChecker(AbstractChapterChecker):
+    """Xbiquge checker"""
+
+    def get_latest_chapter_list(self) -> List[Chapter]:
+        """Get latest chapter list from xbiquge
+
+        Returns:
+            List[Chapter]: latest chapter list
+        """
+        soup = self.get_latest_soup()
+        if soup is None:
+            return []
+        a_list = list(soup.find("div", id="list").findAll("a"))
+        chapter_list = []
+        for chapter_tag in a_list:
+            chapter_title = chapter_tag.text
+            chapter_path = chapter_tag["href"]
+            chapter_url = urljoin(self.check_url, chapter_path)
+            chapter_list.append(Chapter(title=chapter_title, url=chapter_url))
+
+        # Remvoe duplicated
+        chapter_list = list(set(chapter_list))
+
+        # Sort by chapter url
+        chapter_list.sort(key=lambda x: x.url)
+        return chapter_list
