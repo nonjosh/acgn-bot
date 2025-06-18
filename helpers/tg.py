649fde25ea5a9d59cdd53601ad47a79177470448
nonjosh <nonjosh@gmail.com>
2025-06-19 01:22:22 +0800

    refactor retry mechanism for Telegram message sending

--- helpers/tg.py
+++ helpers/tg.py
@@ -1,5 +1,4 @@
 """Telegram Helper"""
-import asyncio
 import os
 
 import telegram
@@ -15,8 +14,6 @@ TOKEN = os.environ.get("TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
 logger = get_logger(__name__)
-
-MAX_RETRIES = 3
 
 
 class TgHelper:
@@ -75,32 +72,13 @@ class TgHelper:
             reply_markup = telegram.InlineKeyboardMarkup([[url_button]])
 
         # Send message
-        retries = 1
-        success = False
-        while not success and retries <= MAX_RETRIES:
-            try:
-                # Create a new instance of the telegram.Bot class
-                bot = telegram.Bot(token=self.token)
-                await bot.send_message(
-                    chat_id=self.chat_id,
-                    text=content,
-                    parse_mode=parse_mode,
-                    reply_markup=reply_markup,
-                )
-                success = True
-                return
-            except telegram.error.TelegramError as err:
-                wait = retries * 30
-                logger.error("Error occurs for %s: %s", content, err)
-                logger.error(
-                    "Waiting %i secs and re-trying... (%i/%i)",
-                    wait,
-                    retries,
-                    MAX_RETRIES,
-                )
-                await asyncio.sleep(wait)
-                retries += 1
-        logger.error("Failed to send message after %i retries", MAX_RETRIES)
+        bot = telegram.Bot(token=self.token)
+        await bot.send_message(
+            chat_id=self.chat_id,
+            text=content,
+            parse_mode=parse_mode,
+            reply_markup=reply_markup,
+        )
 
     async def list_config(self, update: Update, _: CallbackContext) -> None:
         """Send a message when the command /list_config is issued."""
